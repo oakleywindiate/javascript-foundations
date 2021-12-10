972f8386723d6880ecd5bed4632acbdcfbb367e1
Oakley Windiate <oakley.windiate@gmail.com>
2021-12-10 10:51:41 -0700

    Add unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,14 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color, isWhite, says) {
     this.name = name;
-    this.color = color;
+    this.color = color || "white";
+    this.isWhite = function() {
+      return false;
+    }
+    this.says = function(quote) {
+      return "**;* " + quote + " *;**";
+      }
+    }
   }
-}
 
 module.exports = Unicorn;
