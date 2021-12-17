f581a1b1609bbf1b9f2b2449c0675f1650e6b77d
Oakley Windiate <oakley.windiate@gmail.com>
2021-12-16 20:17:24 -0700

    Add hobbit-work-in-progress

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,6 +1,30 @@
 class Hobbit {
-  constructor(hobbit) {
+  constructor(hobbit, age, adult, old, hasRing) {
     this.name = hobbit.name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+    this.hasRing = true;
+  }
+  celebrateBirthday() {
+    this.age++
+    if (this.age > 32) {
+    this.adult = true;
+    }
+    if (this.age >= 101) {
+      this.old = true;
+    }
+  }
+  getRing() {
+    if (this.name === "Frodo") {
+      this.hasRing === true;
+    }
+    if (this.hasRing === true) {
+      return "Here is the ring!"
+    }
+    if (this.hasRing === false) {
+      return "You can\'t have it!"
+    }
   }
 }
 
